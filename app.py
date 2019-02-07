ae67954631bdfc054562a9f7eb4fe4f65f6ab5df
MrGi89 <grzegorz.wojdalski@gmail.com>
2019-02-07 20:53:25 +0100

    - Adds SQL queries - Packs deactivate buttons functions to one - Packs window configuration functions to one - Changes docs - Changes handling of add, update, delete tree item functions - Adds refresh_budget_page function that handles deactivation of update and delete buttons, refreshes trees and current balance

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from datetime import date
 import tkinter as tk
 from tkinter import ttk
+from tools import db_execute
 
 LARGE_FONT = ("verdana", 16)
 MEDIUM_FONT = ("verdana", 12)
@@ -67,11 +68,12 @@ class BudgetPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.budget = 2000
-        self.parent = parent
 
-        back = tk.Button(self, text="< back", command=lambda: controller.show_frame(StartPage))
-        balance_label = tk.Label(self, text='Current balance: {}'.format(self.budget), font=LARGE_FONT)
+        # SETTING ELEMENTS
+
+        self.parent = parent
+        self.back = tk.Button(self, text="< back", command=lambda: controller.show_frame(StartPage))
+        self.balance_label = tk.Label(self, font=LARGE_FONT)
         self.exp_tree = ttk.Treeview(self, columns=('id', 'Expense', 'Amount', 'Date'), show='headings',
                                      selectmode='browse')
         self.exp_tree.heading('#1', text='id', anchor=tk.CENTER)
@@ -84,6 +86,13 @@ class BudgetPage(tk.Frame):
         self.exp_tree.column('#4', width=100, stretch=tk.NO, anchor=tk.CENTER)
         self.exp_tree["displaycolumns"] = ('Expense', 'Amount', 'Date')
         self.exp_tree.bind('<ButtonRelease-1>', self.activate_exp_button)
+        self.exp_add_button = tk.Button(self, text="Add expense", command=lambda: self.add_window(title='Add expense',
+                                                                                                  revenue_type='expense'))
+        self.exp_upd_button = tk.Button(self, text="Update expense", state='disabled',
+                                        command=lambda: self.update_window(title='Update expense',
+                                                                           item=self.exp_tree.item(self.exp_tree.focus())))
+        self.exp_del_button = tk.Button(self, text="Delete expense", state='disabled',
+                                        command=lambda: self.delete_window(item=self.exp_tree.item(self.exp_tree.focus())))
 
         self.inc_tree = ttk.Treeview(self, columns=('id', 'Income', 'Amount', 'Date'), show='headings',
                                      selectmode='browse')
@@ -97,28 +106,19 @@ class BudgetPage(tk.Frame):
         self.inc_tree.column('#4', width=100, stretch=tk.NO, anchor=tk.CENTER)
         self.inc_tree["displaycolumns"] = ('Income', 'Amount', 'Date')
         self.inc_tree.bind('<ButtonRelease-1>', self.activate_inc_button)
-
-        self.exp_tree.insert('', 'end', iid=1, values=(1, 'Rent', '1200', '2019-01-01'))
-        self.exp_tree.insert('', 'end', text='', values=(1, 'Rent2', '2200', '2019-01-02'))
-        self.exp_tree.insert('', 'end', text='', values=(1, 'Rent3', '200', '2019-01-03'))
-        self.inc_tree.insert('', 'end', text='', values=(1, 'Salary', '2200', '2019-01-04'))
-        self.inc_tree.insert('', 'end', text='', values=(1, 'Salary2', '100', '2019-01-05'))
-        self.inc_tree.insert('', 'end', text='', values=(1, 'Salary3', '1500', '2019-01-06'))
-
-        self.exp_add_button = tk.Button(self, text="Add expense", command=self.add_exp_window)
-        self.exp_upd_button = tk.Button(self, text="Update expense", state='disabled',
-                                        command=lambda: self.update_exp_window(self.exp_tree.focus()))
-        self.exp_del_button = tk.Button(self, text="Delete expense", state='disabled',
-                                        command=self.delete_exp_window)
-
-        self.inc_add_button = tk.Button(self, text="Add income", command=lambda: self.add_inc_window())
+        self.inc_add_button = tk.Button(self, text="Add income", command=lambda: self.add_window(title='Add income',
+                                                                                                 revenue_type='income'))
         self.inc_upd_button = tk.Button(self, text="Update income", state='disabled',
-                                        command=lambda: self.update_inc_window(self.inc_tree.focus()))
+                                        command=lambda: self.update_window(title='Update income',
+                                                                           item=self.inc_tree.item(self.inc_tree.focus())))
         self.inc_del_button = tk.Button(self, text="Delete income", state='disabled',
-                                        command=self.delete_inc_window)
+                                        command=lambda: self.delete_window(item=self.inc_tree.item(self.exp_tree.focus())))
+        self.refresh_budget_page()
 
-        back.grid(row=0, column=0, sticky='w', padx=5, pady=5)
-        balance_label.grid(row=0, column=1, sticky='e', padx=5, pady=5)
+        # STYLING
+
+        self.back.grid(row=0, column=0, sticky='w', padx=5, pady=5)
+        self.balance_label.grid(row=0, column=1, sticky='e', padx=5, pady=5)
         self.exp_tree.grid(row=1, column=0, sticky='nsew', padx=5, pady=5)
         self.inc_tree.grid(row=1, column=1, sticky='nsew', padx=5, pady=5)
         self.exp_add_button.grid(row=2, column=0, sticky='nsew', padx=5)
@@ -128,10 +128,39 @@ class BudgetPage(tk.Frame):
         self.inc_upd_button.grid(row=3, column=1, sticky='nsew', padx=5)
         self.inc_del_button.grid(row=4, column=1, sticky='nsew', padx=5)
 
+    def refresh_budget_page(self):
+        """
+        Refreshes budget page data, deactivates buttons, inserts correct values to trees and sets actual balance
+        :return: None
+        """
+        # CLEARS TREE
+        self.exp_tree.delete(*self.exp_tree.get_children())
+        self.inc_tree.delete(*self.inc_tree.get_children())
+        self.deactivate_buttons()
+        # RETRIEVES AND ADDS ACTUAL ELEMENTS TO TREES
+        revenues = db_execute(sql='''SELECT * FROM revenues ORDER BY create_date DESC, amount DESC;''')
+        for revenue in revenues:
+            if revenue['revenue_type'] == 'expense':
+                self.exp_tree.insert('', 'end', values=(revenue['id'],
+                                                        revenue['name'],
+                                                        revenue['amount'],
+                                                        revenue['create_date']))
+            else:
+                self.inc_tree.insert('', 'end', values=(revenue['id'],
+                                                        revenue['name'],
+                                                        revenue['amount'],
+                                                        revenue['create_date']))
+        # SETS ACTUAL BALANCE
+        budget = db_execute(
+            sql='''SELECT ROUND(SUM(CASE WHEN revenue_type='income' THEN amount ELSE 0 END) - 
+                          SUM(CASE WHEN revenue_type='expense' THEN amount ELSE 0 END), 2) AS balance
+                   FROM revenues;''')
+        self.balance_label['text'] = 'Current balance: {}'.format(budget[0]['balance'])
+
     def activate_exp_button(self, event):
         """
-        Event handler that enables usage of update and delete buttons only after selecting one of items in expenses list
-        :param event:
+        Event handler that enables usage of update and delete buttons only after selecting one of expenses tree element
+        :param event: not used
         :return: None
         """
         if self.exp_tree.focus() == '':
@@ -141,8 +170,8 @@ class BudgetPage(tk.Frame):
 
     def activate_inc_button(self, event):
         """
-        Event handler that enables usage of update and delete buttons only after selecting one of items in incomes list
-        :param event:
+        Event handler that enables usage of update and delete buttons only after selecting one of incomes tree element
+        :param event: not used
         :return: None
         """
         if self.inc_tree.focus() == '':
@@ -150,88 +179,53 @@ class BudgetPage(tk.Frame):
         self.inc_upd_button.configure(state='normal')
         self.inc_del_button.configure(state='normal')
 
-    def deactivate_exp_button(self):
+    def deactivate_buttons(self):
         """
-        Function used to deactivate update and delete buttons after deleting items in expenses list
+        Function used to deactivate update and delete buttons after deleting or updating trees elements
         :return: None
         """
         self.exp_upd_button.configure(state='disabled')
         self.exp_del_button.configure(state='disabled')
-
-    def deactivate_inc_button(self):
-        """
-        Function used to deactivate update and delete buttons after deleting items in incomes list
-        :return:
-        """
         self.inc_upd_button.configure(state='disabled')
         self.inc_del_button.configure(state='disabled')
 
-    def add_exp_window(self):
+    def add_window(self, title, revenue_type):
         """
-        Handles configuration of window used to add new item to expenses list
+        Handles configuration of window used to add new tree element
+        :param title: (str) used to configure window title
+        :param revenue_type: (str) type of revenue (income, expense)
         :return: None
         """
         window = AddUpdateElementWindow(self, padx=10, pady=10)
-        window.title('Add expense')
-        window.save['command'] = window.add_exp
-        window.date_entry.insert(0, str(date.today()))
-
-    def add_inc_window(self):
-        """
-        Handles configuration of window used to add new item to incomes list
-        :return: None
-        """
-        window = AddUpdateElementWindow(self, padx=10, pady=10)
+        window.title(title)
+        window.title()
         window.title('Add income')
-        window.save['command'] = window.add_inc
         window.date_entry.insert(0, str(date.today()))
+        window.save['command'] = lambda: window.add_revenue(revenue_type)
 
-    def update_exp_window(self, iid):
+    def update_window(self, title, item):
         """
-        Handles configuration of window used to update item selected from expenses list
-        :param iid: (int) unique number that represents position of item in list
+        Handles configuration of window used to update trees element
+        :param title: (str) used to configure window title
+        :param item: (dict) stores selected item values
         :return: None
         """
         window = AddUpdateElementWindow(self, padx=20, pady=20)
-        window.title('Update expense')
-        window.save['command'] = lambda: window.update_exp(iid)
-
-        item = self.exp_tree.item(iid)
+        window.title(title)
         window.name_entry.insert(0, item['values'][1])
         window.amount_entry.insert(0, item['values'][2])
         window.date_entry.insert(0, item['values'][3])
+        window.save['command'] = lambda: window.update_revenue(revenue_id=item['values'][0])
 
-    def update_inc_window(self, iid):
+    def delete_window(self, item):
         """
-        Handles configuration of window used to update item selected from incomes list
-        :param iid: (int) unique number that represents position of item in list
-        :return: None
-        """
-        window = AddUpdateElementWindow(self, padx=20, pady=20)
-        window.title('Update income')
-        window.save['command'] = lambda: window.update_inc(iid)
-        item = self.inc_tree.item(iid)
-        window.name_entry.insert(0, item['values'][1])
-        window.amount_entry.insert(0, item['values'][2])
-        window.date_entry.insert(0, item['values'][3])
-
-    def delete_exp_window(self):
-        """
-        Handles configuration of window used to delete item from expenses list
+        Handles configuration of window used to delete tree element
+        :param item: (dict) stores selected item values
         :return: None
         """
         window = DeleteElementWindow(self, padx=10, pady=10)
         window.title('Delete element?')
-        window.delete['command'] = lambda: window.delete_exp(self.exp_tree.focus())
-
-    def delete_inc_window(self):
-        """
-        Handles configuration of window used to delete item from incomes list
-        :return: None
-        """
-        window = DeleteElementWindow(self, padx=10, pady=10)
-        window.title('Delete element?')
-        window.delete['command'] = lambda: window.delete_inc(self.inc_tree.focus())
+        window.delete['command'] = lambda: window.delete_revenue(revenue_id=item['values'][0])
 
 
 class AddUpdateElementWindow(tk.Toplevel):
@@ -258,40 +252,34 @@ class AddUpdateElementWindow(tk.Toplevel):
         self.cancel.grid(row=3, column=1, pady=5, sticky='e')
         self.save.grid(row=3, column=2, pady=5, sticky='e')
 
-    def add_exp(self):
+    def add_revenue(self, revenue_type):
+        """
+        Adds new revenue to DB and refreshes trees elements
+        :param revenue_type: (str) type of revenue (income, expense)
+        :return: None
+        """
         # todo validation
         name = self.name_entry.get()
         amount = self.amount_entry.get()
         exp_date = self.date_entry.get()
-        self.parent.exp_tree.insert('', 'end', values=(1, name, amount, exp_date))
+        db_execute(sql='''INSERT INTO revenues VALUES (NULL, ?, ?, ?, ?);''',
+                   variables=(name, revenue_type, amount, exp_date))
+        self.parent.refresh_budget_page()
         self.destroy()
 
-    def update_exp(self, iid):
+    def update_revenue(self, revenue_id):
+        """
+        Updates revenue selected by user and refreshes trees elements
+        :param revenue_id: (int) ID of revenue stored in DB
+        :return: None
+        """
         # todo validation
         name = self.name_entry.get()
         amount = self.amount_entry.get()
         exp_date = self.date_entry.get()
-        index = self.parent.exp_tree.index(iid)
-        self.parent.exp_tree.delete(iid)
-        self.parent.exp_tree.insert('', index, iid=iid, values=(1, name, amount, exp_date))
-        self.destroy()
-
-    def add_inc(self):
-        # todo validation
-        name = self.name_entry.get()
-        amount = self.amount_entry.get()
-        inc_date = self.date_entry.get()
-        self.parent.inc_tree.insert('', 'end', values=(1, name, amount, inc_date))
-        self.destroy()
-
-    def update_inc(self, iid):
-        # todo validation
-        name = self.name_entry.get()
-        amount = self.amount_entry.get()
-        inc_date = self.date_entry.get()
-        index = self.parent.inc_tree.index(iid)
-        self.parent.inc_tree.delete(iid)
-        self.parent.inc_tree.insert('', index, iid=iid, values=(1, name, amount, inc_date))
+        db_execute(sql='''UPDATE revenues SET name=?, amount=?, create_date=? WHERE id=?;''',
+                   variables=(name, amount, exp_date, revenue_id))
+        self.parent.refresh_budget_page()
         self.destroy()
 
 
@@ -309,17 +297,19 @@ class DeleteElementWindow(tk.Toplevel):
         self.cancel.grid(row=1, column=0, pady=5, sticky='e')
         self.delete.grid(row=1, column=1, pady=5, sticky='e')
 
-    def delete_exp(self, iid):
-        self.parent.exp_tree.delete(iid)
-        self.parent.deactivate_exp_button()
+    def delete_revenue(self, revenue_id):
+        """
+        Deletes revenue from DB and refreshes trees elements
+        :param revenue_id: (int) ID of revenue stored in DB
+        :return: None
+        """
+        db_execute(
+            sql='''DELETE FROM revenues WHERE id=?;''',
+            variables=(revenue_id, ))
+        self.parent.refresh_budget_page()
         self.destroy()
 
-    def delete_inc(self, iid):
-        self.parent.inc_tree.delete(iid)
-        self.parent.deactivate_inc_button()
-        self.destroy()
 
-        
 if __name__ == "__main__":
     app = App()
     app.mainloop()
--- tools.py
+++ tools.py
@@ -1,9 +1,17 @@
 import sqlite3
 
 
+def dict_factory(cursor, row):
+    result = dict()
+    for idx, col in enumerate(cursor.description):
+        result[col[0]] = row[idx]
+    return result
+
+
 def db_execute(sql, variables=tuple()):
 
     conn = sqlite3.connect('budget.sqlite')
+    conn.row_factory = dict_factory
     c = conn.cursor()
     c.execute(sql, variables)
     result = c.fetchall()
